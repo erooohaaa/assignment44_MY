e6fc8acea2808ce3ad5a26cca9a5e6b72e4249ae
Yerkanat <erkanatm249@gmail.com>
2025-05-18 16:38:25 +0500

    \

--- src/Vertex.java
+++ src/Vertex.java
@@ -1,8 +1,29 @@
-public class Vertex<V>{
+import java.util.*;
+
+public class Vertex<V> {
     private V data;
     private Map<Vertex<V>, Double> adjacentVertices;
 
-    public void addAdjacentVertices(Vertex<V> v){
-        
+    public Vertex(V data) {
+        this.data = data;
+        this.adjacentVertices = new HashMap<>();
+    }
+
+    public void addAdjacentVertex(Vertex<V> destination, double weight) {
+        adjacentVertices.put(destination, weight);
+    }
+
+    public V getData() {
+        return data;
+    }
+
+    public Map<Vertex<V>, Double> getAdjacentVertices() {
+        return adjacentVertices;
+    }
+
+
+    @Override
+    public String toString() {
+        return data.toString(); // Выводим содержимое data вместо хэш-кода
     }
 }
