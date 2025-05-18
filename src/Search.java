4dedbd15981ec72a8b73d5f109c09df067d8a422
Yerkanat <erkanatm249@gmail.com>
2025-05-18 16:38:18 +0500

    \

--- src/Search.java
+++ src/Search.java
@@ -1,2 +1,12 @@
-public class Search {
+import java.util.*;
+public abstract class Search<V> {
+    protected WeightedGraph<V> graph;
+    protected Vertex<V> start;
+
+    public Search(WeightedGraph<V> graph, Vertex<V> start) {
+        this.graph = graph;
+        this.start = start;
+    }
+
+    public abstract List<Vertex<V>> pathTo(Vertex<V> destination);
 }
